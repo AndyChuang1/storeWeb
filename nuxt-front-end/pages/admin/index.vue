eec67f295792c38122c7ab60dff79bc0da777794
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2019-03-19 20:21:30 +0800

    add types function finished

--- nuxt-front-end/pages/admin/index.vue
+++ nuxt-front-end/pages/admin/index.vue
@@ -4,19 +4,22 @@
       <main-header class="mb-3" title="商品管理"></main-header>
     </div>
     <div style="display:inline-block">
-      <vs-button color="primary" type="border" icon="add" to='admin/add-product' >新增產品</vs-button>
+      <vs-button color="primary" type="border" icon="add" to="admin/add-product">新增產品</vs-button>
     </div>
-     <div style="display:inline-block">
-      <vs-button color="primary" type="border" icon="add" @click="activePrompt = true" >新增類別</vs-button>
-       <vs-prompt
-      @vs-cancel="types=''"
-      @vs-accept="updateTypes"
-      :vs-active.sync="activePrompt">
-       <div class="con-exemple-prompt">
-          Enter the security code
-         <vs-input placeholder="Code" vs-placeholder="Code" v-model="types"/>
-       </div>
-     </vs-prompt>
+    <div style="display:inline-block">
+      <vs-button color="primary" type="border" icon="add" @click="activePrompt = true">新增類別</vs-button>
+      <vs-prompt
+        @vs-cancel="types=''"
+        @vs-accept="updateTypes"
+        :vs-active.sync="activePrompt"
+        vs-title="新增類別"
+        vs-accept-text="新增"
+        vs-cancel-text="取消"
+      >
+        <div class="con-exemple-prompt">輸入類別 :
+          <vs-input placeholder="Types" v-model="types"/>
+        </div>
+      </vs-prompt>
     </div>
 
     <div class="mgt-content">
@@ -50,6 +53,7 @@
 </template>
 <script>
 import MainHeader from "@/components/mainheader";
+import { mapGetters } from "vuex";
 export default {
   name: "admin",
   layout: "admin",
@@ -57,13 +61,15 @@ export default {
   data() {
     return {
       productList: [],
-      activePrompt:false,
-      types:'',
-
+      activePrompt: false,
+      types: ""
     };
   },
   components: {
     MainHeader
+  },
+  computed: {
+    ...mapGetters({ getToken: "auth/getToken" })
   },
   methods: {
     initProduct() {
@@ -82,8 +88,36 @@ export default {
           console.log(error);
         });
     },
-    updateTypes(){
-      console.log("updateTypes")
+    updateTypes() {
+      const token = this.getToken;
+      this.$axios.post(
+        "/apipost/types",
+        {
+          name: this.types
+        },
+        {
+          headers: {
+            "x-access-token": token
+          }
+        }
+      )
+        .then(res => {
+          this.$vs.notify({
+            title: "新增成功",
+            text: "類別以更新",
+            color: "success"
+          });
+        })
+        .catch(err => {
+          const { status } = err.response;
+          if (status == 400) {
+            this.$vs.notify({
+              title: "新增失敗",
+              text: "此類別已存在!",
+              color: "danger"
+            });
+          }
+        });
     }
   },
   created() {
