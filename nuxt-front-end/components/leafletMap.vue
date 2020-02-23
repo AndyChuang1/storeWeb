bc0cac3e4feeb47437832d677b2d730f3b548d92
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-02-23 20:12:49 +0800

    use leaflet MAP instead of google MAP

---
+++ nuxt-front-end/components/leafletMap.vue
@@ -0,0 +1,45 @@
+<template>
+  <div id="map-wrap" style="height: 55vh">
+    <no-ssr>
+      <l-map :zoom="16" :center="[25.059768, 121.530394]">
+        <l-tile-layer
+          url="http://{s}.tile.osm.org/{z}/{x}/{y}.png"
+        ></l-tile-layer>
+        <l-marker name="test" :lat-lng="[25.059822, 121.530391]">
+          <l-popup
+            ><h2 class="title">永盛中藥行</h2>
+            <div class="content">
+              <img
+                :src="require('@/assets/store.jpg')"
+                alt="Store"
+                width="250px"
+              />
+              <p style="margin-top:15px">
+                店鋪地址：(104)台北市中山區吉林路191號
+              </p>
+              <p>
+                連絡電話：<a
+                  ><a href="tel:+886-2-25310309">(02)-2531-0309</a></a
+                >
+              </p>
+              <p>傳真號碼：(02)-2531-0278</p>
+              <p>營業時間：週一至週日 早上10:00~晚上23:00</p>
+            </div></l-popup
+          >
+        </l-marker>
+      </l-map>
+    </no-ssr>
+  </div>
+</template>
+<script>
+export default {
+  name: "leafletMap",
+  data() {
+    return {
+      msg: "© Copyright 2017, Geker Corporation"
+    };
+  }
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="scss" scoped></style>
--- nuxt-front-end/nuxt.config.js
+++ nuxt-front-end/nuxt.config.js
@@ -1,87 +1,96 @@
-const pkg = require('./package')
-const webpack = require('webpack')
-const bodyParser = require('body-parser')
-const session = require('express-session')
-
+const pkg = require("./package");
+const webpack = require("webpack");
+const bodyParser = require("body-parser");
+const session = require("express-session");
 
 module.exports = {
-  mode: 'universal',
-
+  mode: "universal",
+  server: {
+    port: 3000, // default: 3000
+    host: "0.0.0.0" // default: localhost
+  },
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: '永盛堂中藥行',
+    title: "永盛堂中藥行",
     meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: "永盛堂中藥行，提供中藥材，順天本草、勝昌食品、蜜餞、保健食品等等周邊商品。【電話】2531-0309 【地址】中山區吉林路191號." }
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
+      {
+        hid: "description",
+        name: "description",
+        content:
+          "永盛堂中藥行，提供中藥材，順天本草、勝昌食品、蜜餞、保健食品等等周邊商品。【電話】2531-0309 【地址】中山區吉林路191號."
+      }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.png' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/earlyaccess/cwtexkai.css' }
+      { rel: "icon", type: "image/x-icon", href: "/favicon.png" },
+      {
+        rel: "stylesheet",
+        href: "https://fonts.googleapis.com/earlyaccess/cwtexkai.css"
+      }
     ],
     script: [
-      { src: 'https://maps.googleapis.com/maps/api/js?key=AIzaSyAP8uxMTg1l4APwuezlZXqYhTf0J4HeHB0' },
+      {
+        src:
+          "https://maps.googleapis.com/maps/api/js?key=AIzaSyAP8uxMTg1l4APwuezlZXqYhTf0J4HeHB0"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: { color: '#fff' },
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#fff" },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
     "~/node_modules/bootstrap/dist/css/bootstrap.css",
     "~/node_modules/material-icons/iconfont/material-icons.css"
-
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
+   ** Plugins to load before mounting the App
+   */
   plugins: [
     "~plugins/bootstrap.js",
-    { src: '~/plugins/libs/jquery.textillate.js', ssr: false },
-    { src: '~/plugins/libs/jquery.lettering.js', ssr: false },
+    { src: "~/plugins/libs/jquery.textillate.js", ssr: false },
+    { src: "~/plugins/libs/jquery.lettering.js", ssr: false },
     { src: "~plugins/vue-pagination-2.js", ssr: false },
     { src: "~plugins/vuesax.js", ssr: false },
-    '~/plugins/axios.js'
-
+    "~/plugins/axios.js"
   ],
   /*
-  ** Add server middleware
-  ** Nuxt.js uses `connect` module as server
-  ** So most of express middleware works with nuxt.js server middleware
-  */
+   ** Add server middleware
+   ** Nuxt.js uses `connect` module as server
+   ** So most of express middleware works with nuxt.js server middleware
+   */
   serverMiddleware: [
     // body-parser middleware
     bodyParser.json(),
     bodyParser.urlencoded({ extended: true }),
     // session middleware
     session({
-      secret: 'super-secret-key',
+      secret: "super-secret-key",
       resave: false,
       saveUninitialized: false,
-      cookie: { maxAge: 1000*60*60*3 }
+      cookie: { maxAge: 1000 * 60 * 60 * 3 }
     }),
     // Api middleware
-    '~/server/api/index.js'
+    "~/server/api/index.js"
   ],
   env: {
-    devUrl: 'http://localhost:8080',
-    prodUrl: 'http://localhost:3000'
+    devUrl: "http://localhost:8080",
+    prodUrl: "http://localhost:3000"
   },
 
   /*
-  ** Nuxt.js modules
-  */
-  // modules: [
-  //   '@nuxtjs/proxy',
-  // ],
+   ** Nuxt.js modules
+   */
+  modules: ["nuxt-leaflet"],
   // proxy: {
   //   '/api': {
   //     target: 'https://example.com/',
@@ -92,26 +101,28 @@ module.exports = {
   //   }
   // },
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
     /*
-   ** You can extend webpack config here
-   */
+     ** You can extend webpack config here
+     */
 
-    vendor: ['jquery', 'bootstrap', 'textillate', 'lettering', 'vue-pagination-2'],
+    vendor: [
+      "jquery",
+      "bootstrap",
+      "textillate",
+      "lettering",
+      "vue-pagination-2"
+    ],
     plugins: [
       // set shortcuts as global for bootstrap
       new webpack.ProvidePlugin({
-        $: 'jquery',
-        jQuery: 'jquery',
-        'window.jQuery': 'jquery'
+        $: "jquery",
+        jQuery: "jquery",
+        "window.jQuery": "jquery"
       })
     ],
-    extend(config, ctx) {
-
-    }
+    extend(config, ctx) {}
   }
-
-}
-
+};
