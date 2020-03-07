d4d481eefbb5e8b6b157732b806b46a5f5394029
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2020-03-07 21:12:45 +0800

    add privacy page and adjust footer link

--- nuxt-front-end/components/footer.vue
+++ nuxt-front-end/components/footer.vue
@@ -1,10 +1,10 @@
 <template>
-
-  <footer class="container"
-      id="main-footer">
-      <span>&copy; 永盛堂中藥行版權所有 &middot; <a href="#">Privacy</a> &middot; <a href="#">Terms</a></span>
-    </footer>
-
+  <footer class="container" id="main-footer">
+    <span
+      >&copy; 永盛堂中藥行版權所有 &middot;
+      <a href="/privacy">Privacy</a> &middot; <a href="/privacy">Terms</a></span
+    >
+  </footer>
 </template>
 <script>
 export default {
@@ -17,19 +17,18 @@ export default {
 };
 </script>
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style lang='scss'scoped>
-$break-mobile:480px;
+<style lang="scss" scoped>
+$break-mobile: 480px;
 #main-footer {
-    position: absolute;
-   left: 0;
-   bottom: -2rem;
-   right: 0;
-   width: 100%;
-   padding: 1rem;
-   text-align: center;
-   @media screen and(max-width: $break-mobile) {
+  position: absolute;
+  left: 0;
+  bottom: -2rem;
+  right: 0;
+  width: 100%;
+  padding: 1rem;
+  text-align: center;
+  @media screen and(max-width: $break-mobile) {
     position: relative;
+  }
 }
-}
-
 </style>
