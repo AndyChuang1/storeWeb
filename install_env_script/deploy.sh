75ec69d147402a14a1d0c5bf2a4efec02aa8e49c
Andychuang <bboy81905@gmail.com>
2020-08-31 17:28:56 +0800

    adjust rmi path

--- install_env_script/deploy.sh
+++ install_env_script/deploy.sh
@@ -5,7 +5,7 @@ sudo docker-compose down
 
 echo Remove container and Image 
 sudo docker rmi -f bboy81905/storeweb_api-server
-sudo docker rmi -f bboy81905/bboy81905/storeweb_nuxt-server
+sudo docker rmi -f bboy81905/storeweb_nuxt-server
 
 echo Start pulling and running
 sudo docker-compose up -d
