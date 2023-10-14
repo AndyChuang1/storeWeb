bd88035207f055a5de0c24c5bd2df31ee19a3b11
Andychuang <35476023+AndyChuang1@users.noreply.github.com>
2023-10-14 11:31:04 +0800

    feat: move url to env

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,4 @@ echo Remove container and Image
 sudo docker rmi -f bboy81905/storeweb_api-server
 sudo docker rmi -f bboy81905/storeweb_nuxt-server
 
-echo Start pulling and running
-sudo prodUrl=ec2-13-229-86-87.ap-southeast-1.compute.amazonaws.com docker-compose up -d
+
